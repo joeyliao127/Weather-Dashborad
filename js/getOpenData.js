1f5c27b0feddabb772058f8eedbd62ff5165d5be
Melodystart <start99start@gmail.com>
2023-10-17 19:39:15 +0800

    separate function: fetchAPI and contryData

--- js/getOpenData.js
+++ js/getOpenData.js
@@ -1,49 +1,67 @@
 const country = "臺北市"
 const CWB_API_KEY = 'CWA-26B39C5D-83DA-4330-A10E-F13F446664FF';
 
-fetch("https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=" + CWB_API_KEY).then((response) => {
-  return response.json();
-}).then((data) => {
-  const countries = data.records.locations[0].location;
-  let now_hour = new Date().toLocaleString('zh-TW', {
-    hour: 'numeric',
-    hour12: false
-  });
-  now_hour = parseInt(now_hour.replace('時', ''))
+function fetchAPI() {
+  const Data = fetch("https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=" + CWB_API_KEY)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      const countries = data.records.locations[0].location;
+      const result = {}
+      countries.forEach(country => {
+        let countryName = country.locationName
+        result[countryName] = {}
+        result[countryName]["WxValue"] = []
+        let weatherFactor = country.weatherElement;
 
-  countries.forEach(element => {
-    if (element.locationName === country) {
-      console.log(element.locationName)
+        let hour = parseInt(new Date(weatherFactor[0].time[0].startTime).getHours())
 
-      let weather = element.weatherElement;
-      weather.forEach(item => {
+        weatherFactor.forEach(factor => {
+          let index = 0;
+          if (hour >= 18) {
+            index = 1;
+          }
 
-        console.log("欄位：" + item.description)
-        console.log("class：" + item.elementName)
+          let factorName = factor.elementName
+          result[countryName][factorName] = []
+          for (let i = 0; i < 7; i++) {
 
-        let index = 0;
-        if (now_hour >= 18) {
-          index = 1;
-        }
+            if (factor.elementName === "UVI") { index = i; }
+            if (factor.elementName === "Wx") {
+              result[countryName]["WxValue"].push(factor.time[index].elementValue[1].value)
+            }
 
-        for (let i = 0; i < 7; i++) {
-          if (item.elementName === "UVI") { index = i; }
+            let text = factor.time[index].elementValue[0].value
+            if (text === ' ') { text = '0'; }
+            result[countryName][factorName].push(text)
+            index += 2;
+          }
+        })
+      });
+      return result
+    })
+    .catch(error => {
+      console.log(error)
+    });
+  return Data
+}
 
-          console.log(item.time[index].startTime)
+function countryData(country) {
+  const result = {}
+  Data
+    .then((data) => {
+      result["MaxT"] = data[country]["MaxT"]
+      result["MinT"] = data[country]["MinT"]
+      result["PoP12h"] = data[country]["PoP12h"]
+      result["Wx"] = data[country]["Wx"]
+      result["WxValue"] = data[country]["WxValue"]
+    })
+    .catch(error => {
+      console.log(error)
+    });
+  return result
+}
 
-          let details = item.time[index].elementValue
-          details.forEach(d => {
-
-            console.log("值：" + d.value)
-            console.log("單位：" + d.measures)
-
-          })
-          index += 2;
-        }
-        console.log("----------------------")
-      })
-    }
-  });
-
-  // renderRaining(0);
-});
+const Data = fetchAPI();
+countryData(country);
